d348267188667713b6666bb4d3191514dfd598a7
RajaramBhurtel <sabinbhurtel@gmail.com>
2025-05-15 08:57:38 +0545

    new program added

---
+++ ch-11/ComboBoxExample.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+
+public class ComboBoxExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JComboBox Example");
+        JPanel panel=new JPanel();   
+
+        // String arr[] = {"BCA", "BBA", "MCA", "MBA"};
+        // JComboBox<String> cmb = new JComboBox<>(arr);
+        // JList<String> list = new JList<>(arr);
+        // JTextArea txt = new JTextArea("Enter your message here...", 5, 20);
+        // JScrollPane scrollPane = new JScrollPane(txt);
+        // scrollPane.setBounds(50, 50, 200, 100);
+//table
+        String[][] data = {
+            {"Kundan Kumar Jha", "4031", "CSE"},
+            {"Anand Jha", "6014", "IT"}
+        };
+
+        String[] columnNames = {"Name", "Roll Number", "Department"};
+        JTable table = new JTable(data, columnNames);
+
+        // cmb.setBounds(100, 100, 100, 30);
+        // panel.add(cmb);
+        // panel.add(scrollPane);
+        // panel.add(list);
+        panel.add(table);
+        frame.add(panel); 
+
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
--- ch-11/JFrameExe.java
+++ ch-11/JFrameExe.java
@@ -1,7 +1,7 @@
 // Java Swing Program to demonstrate
 // a simple JFrame
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+// import javax.swing.JLabel;
 
 // Driver Class
 public class JFrameExe {
@@ -9,21 +9,20 @@ public class JFrameExe {
     public static void main(String[] args)
     {
         // Create a new JFrame
-        JFrame frame = new JFrame("My First JFrame");
+        JFrame frame = new JFrame();
 
         // Create a label
-        JLabel label
-            = new JLabel("This is label");
+        // JLabel label
+        //     = new JLabel("This is label");
 
-        // Add the label to the frame
-        frame.add(label);
+        // // Add the label to the frame
+        // frame.add(label);
 
         // Set frame properties
         frame.setSize(300,200); // Set the size of the frame
 
         // Close operation
-        frame.setDefaultCloseOperation(
-            JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Make the frame visible
         frame.setVisible(true);
